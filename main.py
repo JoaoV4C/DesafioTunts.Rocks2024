ac0810cbff31aa39cbe7ab9a53aa180ff966d2f8
JoaoV4C <jvalves62@outlook.com>
2024-02-01 01:24:14 -0300

    Fix Directory 2

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def get_credentials(creds):
     if creds and creds.expired and creds.refresh_token:
       creds.refresh(Request())
     else:
-      flow = InstalledAppFlow.from_client_secrets_file("DesafioTunts.Rocks2024./credentials.json", SCOPES)
+      flow = InstalledAppFlow.from_client_secrets_file("DesafioTunts.Rocks2024/credentials.json", SCOPES)
       creds = flow.run_local_server(port=0)
     # Save the credentials for the next run
     with open("token.json", "w") as token:
